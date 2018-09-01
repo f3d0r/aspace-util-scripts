759707bbac6922208c72f137b00523a8336edd6a
Fedor Paretsky <me@f3d0r.com>
2018-09-01 15:30:58 -0700

    Add Format KML to GEOJSON dics util script

---
+++ format_kml_geojson_space_divs/app.js
@@ -0,0 +1,34 @@
+var fs = require('fs');
+var readLine = require('readline');
+var es = require('event-stream');
+
+var output = "";
+var count = 0;
+
+var s = fs.createReadStream('input.txt')
+    .pipe(es.split())
+    .pipe(es.mapSync(function (line) {
+            var firstChar = line.charAt(0);
+            // console.log(line);
+            if (firstChar != "\"" && firstChar != "<") {
+                output += (line.substring(0, line.indexOf(' ')) + "\n");
+                count++;
+            }
+        })
+        .on('error', function (err) {
+            console.log('Error while reading file.', err);
+        })
+        .on('end', function () {
+            writeFile('output.txt', output)
+        })
+    );
+
+function writeFile(fileName, output) {
+    fs.writeFile(fileName, output, function (err) {
+        if (err) {
+            return console.log(err);
+        } else {
+            console.log("FILE SAVED, OUTPUT LINE COUNT: " + count)
+        }
+    });
+}
