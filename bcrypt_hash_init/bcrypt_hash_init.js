85413f55ae09b2e338bb4b9834aa70a516db6d09
Fedor Paretsky <me@f3d0r.com>
2018-09-10 14:15:12 -0700

    Add bcrypt_hash_init script for manual bcrypt database entry

---
+++ bcrypt_hash_init/bcrypt_hash_init.js
@@ -0,0 +1,13 @@
+var bcrypt = require('bcrypt');
+
+var stdin = process.openStdin();
+console.log("Please enter a password on the next line:");
+
+stdin.addListener("data", function (d) {
+    bcrypt.hash(d.toString().trim(), 10, function (err, hash) {
+        for (var x = 0; x <= 10000; x++) {
+            console.log();
+        }
+        console.log(hash)
+    });
+});
